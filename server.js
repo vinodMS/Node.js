e6346ed0b9ada56a54a1bab31ee03eab534ea927
Vinod Sudharshan <vinod@taus.net>
2014-10-07 03:10:20 +0200

    hyper_controls_added

--- server.js
+++ server.js
@@ -12,6 +12,8 @@ var app         = express();
 module.exports  = app;
 var bodyParser  = require('body-parser');
 var morgan      = require('morgan');
+var translationMethodName = '/v2.0/translation/';
+var d = new Date().getTime();
 //app.use(express.logger('dev'));
 
 
@@ -59,6 +61,7 @@ router.route('/translation')
 
     	var translations = new Translations();		// create a new instance of the Translations model
 
+      translations.id              = req.body.id;
       translations.sourceLanguage  = req.body.sourceLanguage;
       translations.targetLanguage  = req.body.targetLanguage;
       translations.source          = req.body.source;
@@ -70,13 +73,54 @@ router.route('/translation')
       //translations.markModified('translationRequests');
       //console.log(req.body.translationRequests);
       //res.statusCode = 201;
+      var url = "http://" + req.headers.host;
+      var links =
 
+		[
+			{
+				"rel": "translation",
+				"href": url + translationMethodName + translations.id,
+				"type": "application/json",
+				"title": "Newly created translation request " + translations.id + " + created on " + " " + d,
+				"verb": "GET"
+			},
+			{
+				"rel": "translation.cancel",
+				"href": url + translationMethodName +"cancel/" + translations.id,
+				"type": "application/json",
+				"verb": "PATCH"
+			},
+			{
+				"rel": "translation.confirm",
+				"href": url + translationMethodName +"reject/" + translations.id,
+				"type": "application/json",
+				"verb": "PATCH"
+			},
+			{
+				"rel": "translation.reject",
+				"href": url + translationMethodName + "confirm/" + translations.id,
+				"type": "application/json",
+				"verb": "PATCH"
+			},
+			{
+				"rel": "translation.reject",
+				"href": url + translationMethodName + "accept/" + translations.id,
+				"type": "application/json",
+				"verb": "PATCH"
+			},
+			{
+				"rel": "translation.patch",
+				"href": url + translationMethodName + translations.id,
+				"type": "application/json",
+				"verb": "PATCH"
+			}
+		];
     	translations.save(function(err) {
     		if (err)  {
           concole.log(err);
           res.status(400).json({status: 'failure'});
         } else {
-          res.status(201).json({ message: 'Translations request succesfully created!' });
+          res.status(201).json({ links: links });
         }
     	});
 
@@ -95,18 +139,18 @@ router.route('/translation')
         });
     });
 
-// on routes that end in /translation/:translations_id
+// on routes that end in /translation/:translations.id
 // this methods will be used to get a specific translations with ID
 // ----------------------------------------------------
-router.route('/translation/:translations_id')
+router.route('/translation/:translations.id')
 
     	// get the translations with that id
     	.get(function(req, res) {
-        		Translations.findById(req.params.translations_id, function(err, translations) {
+        		Translations.findById(req.params.translations.id, function(err, translations) {
         			if (err) {
                 res.status(404).json({status: "Request ID not found"});
               } else
-                if (req.params.translations_id == 'undefined'){
+                if (req.params.translations.id === undefined){
                 res.status(404).json({status: "Request ID not found"});
               } else {
                 res.status(200).json(translations);
@@ -118,7 +162,7 @@ router.route('/translation/:translations_id')
     	.put(function(req, res) {
 
     		// use our translations model to find the translations we want
-    		Translations.findById(req.params.translations_id, function(err, translations) {
+    		Translations.findById(req.params.translations.id, function(err, translations) {
 
     			if (err)
     				res.send(err);
@@ -146,7 +190,7 @@ router.route('/translation/:translations_id')
     	// delete the translations with this id
     	.delete(function(req, res) {
     		Translations.remove({
-    			_id: req.params.translations_id
+    			_id: req.params.translations.id
     		}, function(err, translations) {
     			if (err)
     				res.send(err);
@@ -156,6 +200,123 @@ router.route('/translation/:translations_id')
     		});
     	});
 
+
+// on routes that end in /accept/:translations.id
+// this methods will be used to accept a specific translations with ID
+// ----------------------------------------------------
+router.route('/accept/:translations.id')
+
+// accept the translations with this id
+.put(function(req, res) {
+
+  // use our translations model to find the translations we want
+  Translations.findById(req.params.translations.id, function(err, translations) {
+
+    if (!err){
+      translations.status = 'accepted';
+      translations.updateCounter += 1;
+      // save the translations
+      translations.save(function(err) {
+        if (err) {
+          res.status(500).json({status: "Server Failure"});
+        } else {
+          res.status(200).json({message: "Translations request was succesfully changed"});
+        }
+      });
+    } else {
+      res.status(404).json({message: "Request ID not found"});
+    }
+
+  });
+});
+
+
+// on routes that end in /reject/:translations.id
+// this methods will be used to rejects a specific translations with ID
+// ----------------------------------------------------
+router.route('/reject/:translations.id')
+
+// reject the translations with this id
+.put(function(req, res) {
+
+  // use our translations model to find the translations we want
+  Translations.findById(req.params.translations.id, function(err, translations) {
+
+    if (!err){
+      translations.status = 'rejected';
+      translations.updateCounter += 1;
+      // save the translations
+      translations.save(function(err) {
+        if (err) {
+          res.status(500).json({status: "Server Failure"});
+        } else {
+          res.status(200).json({message: "Translations request was succesfully rejected"});
+        }
+      });
+    } else {
+      res.status(404).json({message: "Request ID not found"});
+    }
+
+  });
+});
+
+// on routes that end in /confirm/:translations.id
+// this methods will be used to confirm a specific translations with ID
+// ----------------------------------------------------
+router.route('/confirm/:translations.id')
+
+// confirm the translations with this id
+.put(function(req, res) {
+
+  // use our translations model to find the translations we want
+  Translations.findById(req.params.translations.id, function(err, translations) {
+
+    if (!err){
+      translations.status = 'confirmed';
+      translations.updateCounter += 1;
+      // save the translations
+      translations.save(function(err) {
+        if (err) {
+          res.status(500).json({status: "Server Failure"});
+        } else {
+          res.status(200).json({message: "Translations request was succesfully changed"});
+        }
+      });
+    } else {
+      res.status(404).json({message: "Request ID not found"});
+    }
+
+  });
+});
+
+// on routes that end in /cancel/:translations.id
+// this methods will be used to cancel a specific translations with ID
+// ----------------------------------------------------
+router.route('/cancel/:translations.id')
+
+// cancel the translations with this id
+.put(function(req, res) {
+
+  // use our translations model to find the translations we want
+  Translations.findById(req.params.translations.id, function(err, translations) {
+
+    if (!err){
+      translations.status = 'cancelled';
+      translations.updateCounter += 1;
+      // save the translations
+      translations.save(function(err) {
+        if (err) {
+          res.status(500).json({status: "Server Failure"});
+        } else {
+          res.status(200).json({message: "Translations request was succesfully changed"});
+        }
+      });
+    } else {
+      res.status(404).json({message: "Request ID not found"});
+    }
+
+  });
+});
 
 app.use('/v2.0', router);
 
--- tests/translationspec.js
+++ tests/translationspec.js
@@ -13,7 +13,7 @@ describe('Request-1', function () {
     .expect(200)
     .end(function (err, res) {
       res.status.should.equal(200);
-      done();
+      done(); //It is typically the case when working with promises that its best to end the chain by calling a method like this.
     });
 
   });
@@ -57,4 +57,36 @@ describe('Request-2', function () {
       done();
     });
   });
+});
+
+describe('Request-3', function () {
+
+  it('should accept the specific translation request',
+  function (done) {
+
+    supertest(app)
+    .put('/v2.0/accept/543304f3fddf1d0bd4000001')
+    .expect(200)
+    .end(function (err, res) {
+      res.status.should.equal(200);
+      res.body.message.should.not.equal(undefined);
+      done();
+    });
+
+  });
+
+  it('should return error due to incorrect id',
+  function (done) {
+
+    supertest(app)
+    .put('/v2.0/accept/12121212')
+    .expect(404)
+    .end(function (err, res) {
+      res.status.should.equal(404);
+      res.body.message.should.not.equal(undefined);
+      done();
+    });
+
+  });
+
 });
